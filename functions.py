eabd8040aff2f0ff86733dc79094aa45c940f883
ayam04 <ayamullahkhan04@gmail.com>
2024-06-13 12:58:27 +0530

    fix

--- functions.py
+++ functions.py
@@ -3,11 +3,14 @@ import json
 import whisper
 import warnings
 import subprocess
+import numpy as np
 from dotenv import load_dotenv
 from langchain_community.llms.huggingface_hub import HuggingFaceHub
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+
 
 load_dotenv()
 
@@ -17,16 +20,35 @@ model = whisper.load_model("base")
 llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.3", model_kwargs={"temperature": 0.5, "max_new_tokens": 30000})
 
 def transcribe_video(video_input):
-    audio_output = "audio.mp3"
+    audio_output = "audio.wav"
     ffmpeg_command = f"ffmpeg -i {video_input} -vn -c:a libmp3lame -b:a 192k {audio_output}"
     subprocess.run(ffmpeg_command, shell=True, check=True)
+    
+    model = whisper.load_model("base")
     response = model.transcribe(audio_output)  
     os.remove(audio_output)
-    return response["text"].strip()
+    
+    total_confidence = 0
+    total_length = 0
+    
+    for segment in response["segments"]:
+        avg_logprob = segment['avg_logprob']
+        confidence = np.exp(avg_logprob)
+        segment_length = segment['end'] - segment['start']
+        total_confidence += confidence * segment_length
+        total_length += segment_length
+    
+    if total_length == 0:
+        return 0
+    
+    average_confidence = total_confidence / total_length
+    return (f"{average_confidence*10:.0f}", response["text"].strip())
 
-def classify_video(transcript):
+def classify_video(conf_transcript):
+    conf, transcript = conf_transcript
+    conf = int(conf)
     prompt = f"""
-    You are a professional English teacher. You are presented with a transcript of a candidate's interview for any role in general. You are supposed to catgeorize the candidate's overall performance on a scale of 1 to 10.
+    You are a professional English teacher. You are presented with a transcript of a candidate's interview for any role in general. You are supposed to categorize the candidate's overall performance on a scale of 1 to 10.
     
     You are also expected to rate the candidate on these metrics on the scale of 1 to 10:
     
@@ -46,14 +68,15 @@ def classify_video(transcript):
         "Fluency": 6,
         "Grammar and Syntax": 5,
         "Vocabulary and Word Choice": 1,
-        "Pronunciation and Accent": 10,
+        "Pronunciation and Accent": 0,
         "Comprehension and Responsiveness": 8
     }}
     This is just the format of the json. Do not send the above json as the response, unless you want to provide the same ratings for the candidate.
     Update the scores in the json response above with the ratings you want to provide for the candidate.
     """
+    
     response = llm(prompt).replace(prompt, "").replace("\n", "").strip()
-
+    
     try:
         json_response = json.loads(response)
     except json.JSONDecodeError as e:
@@ -66,9 +89,12 @@ def classify_video(transcript):
             print(f"Secondary JSON decode error: {e}")
             raise
 
+    json_response["Pronunciation and Accent"] = conf
+
     return json_response
     
 # video_input = "Videos/video.mp4"
 
 # transcript = transcribe_video(video_input)
-# classify_video(transcript)
+# print(transcript)
+# print(classify_video(transcript))
